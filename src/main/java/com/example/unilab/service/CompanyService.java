219c0fe377969fcc709d59a0b83143b13101e886
Bahadir <bahadurmusayev15@gmail.com>
2026-05-26 21:03:58 +0400

    environment variables are already set, entity and repository classes are extended

---
+++ src/main/java/com/example/unilab/service/CompanyService.java
@@ -0,0 +1,14 @@
+package com.example.unilab.service;
+
+import com.example.unilab.repository.CompanyRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class CompanyService {
+    CompanyRepository companyRepository;
+}
